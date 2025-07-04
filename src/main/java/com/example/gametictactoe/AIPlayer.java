1fa32f40697cda1c0ccf9f1a3e0dc4d315e07754
Randarola <140419343+Randarola@users.noreply.github.com>
2025-07-04 19:22:07 +0200

    add bestMove  in AIPlayer

--- src/main/java/com/example/gametictactoe/AIPlayer.java
+++ src/main/java/com/example/gametictactoe/AIPlayer.java
@@ -1,4 +1,14 @@
 package com.example.gametictactoe;
 
 public class AIPlayer {
+    public int[] bestMove(Board.Tile[][] board) {
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                if (board[x][y].text.getText().isEmpty()) {
+                    return new int[]{x, y};
+                }
+            }
+        }
+        return null;
+    }
 }
